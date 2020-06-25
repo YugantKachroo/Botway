891287a70c7d77f531f8c19f90bb961631716d4a
Yugant Kachroo <yugant35@gmail.com>
2020-06-25 14:27:26 +0530

    help command added

--- src/commands/skip.js
+++ src/commands/skip.js
@@ -4,15 +4,17 @@ module.exports = {
   cooldown: 5,
   execute(message) {
     const { channel } = message.member.voice;
-    if (!channel)
+    if (!channel) {
       return message.channel.send(
         "I'm sorry but you need to be in a voice channel to play music!"
       );
+    }
     const serverQueue = message.client.queue.get(message.guild.id);
-    if (!serverQueue)
+    if (!serverQueue) {
       return message.channel.send(
         'There is nothing playing that I could skip for you.'
       );
+    }
     serverQueue.connection.dispatcher.end('Skip command has been used!');
   },
 };
